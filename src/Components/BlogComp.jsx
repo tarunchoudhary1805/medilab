4948a605f9328c2423195c77fafb18072e8ba323
Tarun Choudhary <tarunchoudhary1805@gmail.com>
2021-11-10 19:06:11 +0530

    aded

--- src/Components/BlogComp.jsx
+++ src/Components/BlogComp.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import ReactHtmlParser from "react-html-parser";
+import "../Shared/css/style.css";
 
 const BlogComp = () => {
   const [blogs, setBlogs] = useState([]);
@@ -32,8 +33,11 @@ const BlogComp = () => {
           <div class="row">
             {blogs?.map((item, i) => (
               <>
-                <div class="col-lg-4">
-                  <Link to={{ pathname: "/blog", state: item }}>
+                <div class="col-lg-4 col-md-6 my-2">
+                  <Link
+                    className="text-dark"
+                    to={{ pathname: "/blog", state: item }}
+                  >
                     <div class="member d-flex align-items">
                       <div class="pix">
                         <img src={item.img} class="img-fluid" alt="" />
@@ -41,16 +45,13 @@ const BlogComp = () => {
                       <div class="member-info">
                         <h4>
                           {item.title.length > 40
-                            ? `${item.title.slice(0, 40)}. . .`
+                            ? `${item.title.slice(0, 30)}. . .`
                             : item.title}
                         </h4>
                         {/* <span>M.S.</span> */}
-                        <p style={{ color: "#000" }}>
-                          {item.description.length > 40
-                            ? `${ReactHtmlParser(
-                                item.description.slice(0, 40)
-                              )}. . .`
-                            : ReactHtmlParser(item.description)}
+                        <p className="span">
+                          {ReactHtmlParser(item.description)}{" "}
+                          {/* <span>{`. . .`}</span> */}
                         </p>
                       </div>
                     </div>
--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import ReactHtmlParser from "react-html-parser";
 import { useEffect } from "react";
 import { useLocation } from "react-router";
+import logo from "../img/logo.png";
 
 const Blog = (props) => {
   const location = useLocation();
@@ -26,6 +27,7 @@ const Blog = (props) => {
           <h2> Blog </h2>
         </div>
       </section>
+
       <section id="team" class="team">
         <div class="container" data-aos="fade-up">
           {/* </ */}
@@ -37,22 +39,16 @@ const Blog = (props) => {
                 <div className="card-body">
                   <h5 className="card-title">
                     <div
-                      className="d-flex image"
+                      className="d-flex image justify-content-center"
                       style={{ alignItems: "center" }}
                     >
-                      {/* <img src={heroImg} className="card-img mr-2 " alt="..." /> */}
-                      {/* <div>
-                        <h4>HeaveN Real Estate</h4>
-                        <p
-                          style={{
-                            fontSize: "12px",
-                            padding: 0,
-                            margin: 0,
-                          }}
-                        >
-                          {moment(item.created_at).fromNow()}
-                        </p>
-                      </div> */}
+                      <img
+                        src={logo}
+                        className="img-logo "
+                        // style={{ width: "30%" }}
+                        alt="..."
+                      />
+                      
                     </div>{" "}
                     <div> </div>
                     {/* <div className="d-flex"></div> */}
@@ -72,19 +68,7 @@ const Blog = (props) => {
                       <b>{item.title}</b>
                     </h3>
                     <p className="card-text p-1 para  ">
-                      {!readmore && item.description.length > 40
-                        ? `${ReactHtmlParser(
-                            item.description.slice(0, 40)
-                          )}. . .`
-                        : ReactHtmlParser(item.description)}
-                      <a
-                        onClick={() => setReadmore(!readmore)}
-                        className="btn text-dark"
-                        style={{ border: "none", outline: "none" }}
-                      >
-                        {item.description.length > 100 &&
-                          `${!readmore ? "Read More" : "Read Less"}`}
-                      </a>
+                      {ReactHtmlParser(item.description)}
                     </p>
                   </div>
                 </div>
